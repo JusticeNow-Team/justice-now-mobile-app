f45efc9b12682e6fa029a7fe8d9d8ca2e1715c4d
Sithum <sithumridmal34@gmail.com>
2026-09-14 15:07:41 +0530

    jn-107

--- src/reporter/cases/index.ts
+++ src/reporter/cases/index.ts
@@ -1,4 +1,6 @@
 export { default as MyCasesScreen } from "./MyCasesScreen";
 export { default as CaseDetailScreen } from "./CaseDetailScreen";
+export { default as InformationRequestScreen } from "./InformationRequestScreen";
 export { getReporterCases } from "./getReporterCases";
 export { getReporterCaseDetail } from "./getReporterCaseDetail";
+export { submitInformationResponse } from "./submitInformationResponse";
